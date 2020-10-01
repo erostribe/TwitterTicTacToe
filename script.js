68f5c84be3abd5b7d2235bc89c717867e3e1f5e4
Janet Azehko <Janet@projecterostribe.com>
2020-09-30 22:43:47 -0700

    script.js started. Board,  Human and AI variables created

---
+++ script.js
@@ -0,0 +1,6 @@
+var origBoard: // Keeps track of what is in each square of the board //
+
+const humanPlayer = "O" // Keeps track of the human player as O //
+
+const aiPlayer = "X" // Keeps track of the AI player as X //
+
