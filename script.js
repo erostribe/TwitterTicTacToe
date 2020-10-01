c2c33d9fdb4b03db6aaab6267241a012d87f1548
Janet Azehko <Janet@projecterostribe.com>
2020-10-01 01:55:18 -0700

    winning combo arrays  added to winningCombo function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var origBoard: // Keeps track of what is in each square of the board //
 
-const humanPlayer = "O" // Keeps track of the human player as O //
+const humanPlayer = 'O'; // Keeps track of the human player as O //
 
-const aiPlayer = "X" // Keeps track of the AI player as X //
+const aiPlayer = 'X'; // Keeps track of the AI player as X //
+
+const winningCombo = [] //  Shows the winning combinations for the board //
 
