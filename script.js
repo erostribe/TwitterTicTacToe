4b141e063654766f0906e142dc981ab4110456bb
Janet Azehko <Janet@projecterostribe.com>
2020-10-01 02:03:06 -0700

    winningCombo array added

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const humanPlayer = 'O'; // Keeps track of the human player as O //
 const aiPlayer = 'X'; // Keeps track of the AI player as X //
 
 const winningCombo = [
-
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
 ] //  Shows all the winning combinations for the board //
 
